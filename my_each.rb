fa453e7626611d7639541f7a6a159b467bd92f85
Marco Miranda <marmirand@hotmail.com>
2019-04-06 12:41:46 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,8 @@
-collection = []
-
-def my_each(collection)
-  collection.each do |i|
-    puts i
-end
+def my_each(array)
+  i = 0
+  while i < array.length
+     yield(array[i])
+     i +=1
+   end
+   array
+ end
